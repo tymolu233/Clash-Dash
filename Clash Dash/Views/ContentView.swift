644d04afe0776485c1021a7f9701604a16c92c5c
bin64 <yanmou1996@gmail.com>
2024-12-18 12:35:26 +0800

    chore: update version number to 1.2.1

--- Clash Dash/Views/ContentView.swift
+++ Clash Dash/Views/ContentView.swift
@@ -160,7 +160,7 @@ struct ContentView: View {
                         .cornerRadius(16)
                         
                         // 版本信息
-                        Text("Ver: 1.2.0")
+                        Text("Ver: 1.2.1")
                             .foregroundColor(.secondary)
                             .font(.footnote)
                             .padding(.top, 8)
